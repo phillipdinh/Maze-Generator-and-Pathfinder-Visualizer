32f2c8c2b705560209398f8dd394fe9b933725f2
phillipdinh <phillipdinh11@yahoo.com>
2024-02-17 22:50:33 -0800

    Updated to work with new checkNeighborsSolve function

--- src/solvers/dfs-solve.js
+++ src/solvers/dfs-solve.js
@@ -17,21 +17,21 @@ export default function dfsSolve(startCol, startRow, finishCol, finishRow, maze)
 
 		// backTrack() and mark node as false if no valid neighbors are found
 		if (visitedCount < maze.length * maze[0].length) {
-			const neighbors = getNeighbors(col, row)
-			const validNeighbors = checkNeighborsSolve(col, row, neighbors, maze)
-			const randDir = getRand(validNeighbors)
+			//const neighbors = getNeighbors(col, row)
+			const validNeighbors = checkNeighborsSolve(col, row, maze)
+			const randNeighbor = getRand(validNeighbors)
 
-			if (randDir) {
-				stack.push([col, row])
+			if (randNeighbor) {
+				openNodes.push([col, row])
 				visitedCount = visitedCount + 1
 
-				const [nCol, nRow] = neighbors[randDir]
-				return backTrack(nCol, nRow, maze)
+				//const [nCol, nRow] = neighbors[randDir]
+				return backTrack(randNeighbor.col, randNeighbor.row, maze)
 			}
 			visitedNodesInOrder.push(false)
 
-			if (stack.length > 0) {
-				const lastNode = stack.pop()
+			if (openNodes.length > 0) {
+				const lastNode = openNodes.pop()
 				return backTrack(lastNode[0], lastNode[1], maze)
 			}
 			return
@@ -39,7 +39,7 @@ export default function dfsSolve(startCol, startRow, finishCol, finishRow, maze)
 	}
 
 	const visitedNodesInOrder = []
-	const stack = []
+	const openNodes = []
 	var visitedCount = 0
 
 	backTrack(startCol, startRow, maze)
