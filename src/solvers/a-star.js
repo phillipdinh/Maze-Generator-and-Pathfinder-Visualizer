3908d248cc982910215255d51cd545989fe48d3c
phillipdinh <phillipdinh11@yahoo.com>
2024-02-17 23:13:21 -0800

    Created updateNeighbor function

--- src/solvers/a-star.js
+++ src/solvers/a-star.js
@@ -17,35 +17,27 @@ export default function aStar(startCol, startRow, finishCol, finishRow, maze) {
 
 		const neighbors = checkNeighborsSolve(closestNode.col, closestNode.row, maze)
 
-		/*const validNeighbors = checkNeighborsSolve(
-			closestNode.col,
-			closestNode.row,
-			neighbors,
-			maze
-		)
-        */
-
-		//TODO Check if we can remove the duplicate code
+		//TODO Try remove the duplicate code
 		for (const neighbor of neighbors) {
 			console.log(neighbor)
 			const distance = closestNode.distance + 1
+
 			if (!isNeighborInOpenNodes(neighbor, openNodes)) {
 				openNodes.unshift(neighbor)
-				neighbor.distance = distance
-				neighbor.totalDistance =
-					distance + getManhattan(neighbor, finishRow, finishCol)
-				neighbor.previousNode = closestNode
+				updateNeighbor(neighbor, distance, closestNode, finishCol, finishRow)
 			} else if (distance < neighbor.distance) {
-				neighbor.distance = distance
-				neighbor.totalDistance =
-					distance + getManhattan(neighbor, finishRow, finishCol)
-				neighbor.previousNode = closestNode
+				updateNeighbor(neighbor, distance, closestNode, finishCol, finishRow)
 			}
 		}
 	}
 	return visitedNodesInOrder
 }
 
+function updateNeighbor(neighbor, distance, closestNode, finishCol, finishRow) {
+	neighbor.distance = distance
+	neighbor.totalDistance = distance + getManhattan(neighbor, finishCol, finishRow)
+	neighbor.previousNode = closestNode
+}
 function getManhattan(node, finishCol, finishRow) {
 	return Math.abs(node.col - finishCol) + Math.abs(node.row - finishRow)
 }
