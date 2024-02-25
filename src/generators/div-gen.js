3709deadb213c802a21ef00ddc34054d26802b6d
phillipdinh <phillipdinh11@yahoo.com>
2024-02-24 20:37:02 -0800

    Updated comments

--- src/generators/div-gen.js
+++ src/generators/div-gen.js
@@ -1,84 +1,70 @@
 export default function divGen(maze) {
+	// TODO: better comments
 	function divide(col, row, width, height, orientation) {
 		if (width < 2 || height < 2) return
 
-		// Boolean (True if orientation is horizontal)
 		const horizontal = orientation === "horizontal"
+		const length = horizontal ? width : height
 
-		// col + 0 if (horizontal) is TRUE
-		// col + rand(0 ... width - 2) if FALSE
+		// Choose wall
 		var wx = col + (horizontal ? 0 : Math.floor(Math.random() * (width - 2)))
-
-		// row + 0 if (horizontal) is FALSE
-		// row + rand(0 ... height - 2) if TRUE
 		var wy = row + (horizontal ? Math.floor(Math.random() * (height - 2)) : 0)
 
-		// passage
+		// Choose passage in wall
 		const px = wx + (horizontal ? Math.floor(Math.random() * width) : 0)
 		const py = wy + (horizontal ? 0 : Math.floor(Math.random() * height))
 
-		// length of wall extends entire width or height
-		const length = horizontal ? width : height
-
 		drawMaze(wx, wy, px, py, length, horizontal)
 
+		// One side of wall
 		var nx = col
 		var ny = row
-
 		var w = horizontal ? width : wx - col + 1
 		var h = horizontal ? wy - row + 1 : height
-
 		divide(nx, ny, w, h, chooseOrientation(w, h))
 
+		// Other side of wall
 		nx = horizontal ? col : wx + 1
 		ny = horizontal ? wy + 1 : row
-
 		w = horizontal ? width : col + width - wx - 1
 		h = horizontal ? row + height - wy - 1 : height
-
 		divide(nx, ny, w, h, chooseOrientation(w, h))
 	}
 
 	function drawMaze(wx, wy, px, py, length, horizontal) {
-		// direction
+		// Direction
 		const dx = horizontal ? 1 : 0
 		const dy = horizontal ? 0 : 1
 
 		for (let i = 0; i < length; i++) {
 			if (wx !== px || wy !== py) {
 				if (horizontal) {
-					visitedNodesInOrder.push([maze[wx][wy], "bottom"])
-					visitedNodesInOrder.push([maze[wx][wy + 1], "top"])
+					visitedNodes.push([maze[wx][wy], "bottom"])
+					visitedNodes.push([maze[wx][wy + 1], "top"])
 				} else {
-					visitedNodesInOrder.push([maze[wx][wy], "right"])
-					visitedNodesInOrder.push([maze[wx + 1][wy], "left"])
+					visitedNodes.push([maze[wx][wy], "right"])
+					visitedNodes.push([maze[wx + 1][wy], "left"])
 				}
 			} else {
 				if (horizontal) {
-					visitedNodesInOrder.push([maze[px][py]])
-					visitedNodesInOrder.push([maze[px][py + 1]])
+					visitedNodes.push([maze[px][py]])
+					visitedNodes.push([maze[px][py + 1]])
 				} else {
-					visitedNodesInOrder.push([maze[px][py]])
-					visitedNodesInOrder.push([maze[px + 1][py]])
+					visitedNodes.push([maze[px][py]])
+					visitedNodes.push([maze[px + 1][py]])
 				}
 			}
-
-			// go to next node
 			wx += dx
 			wy += dy
 		}
 	}
 
-	const visitedNodesInOrder = []
-
+	const visitedNodes = []
 	divide(0, 0, 16, 16, chooseOrientation(16, 16))
-
-	return visitedNodesInOrder
+	return visitedNodes
 }
 
-/* Horizontal   if Height is larger
- * Vertical     if Width if larger
- * Return either randomly if equal
+/* Return smaller orientation or random if equal
  */
 function chooseOrientation(width, height) {
 	if (width < height) return "horizontal"
